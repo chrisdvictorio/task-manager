bb6e5656356d0e147531bdd581d99a9bb982fb69
Chris <chrisdvictorio@gmail.com>
2025-09-11 20:46:42 +0800

    extend TaskRepository with JpaRepository

--- backend/src/main/java/com/victorio/taskmanager/repository/TaskRepository.java
+++ backend/src/main/java/com/victorio/taskmanager/repository/TaskRepository.java
@@ -1,4 +1,6 @@
 package com.victorio.taskmanager.repository;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository {
 }
