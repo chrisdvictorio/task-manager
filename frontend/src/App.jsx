dd05290bf935d4808bbd770d5454800266d3e18e
Chris <chrisdvictorio@gmail.com>
2025-09-12 19:44:25 +0800

    install react-router-dom library for routing and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import ErrorPage from "./pages/ErrorPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
